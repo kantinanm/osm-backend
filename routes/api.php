c939470b926738ce4433be92268a83877dba889d
basicgts <kantinanm@nu.ac.th>
2022-08-29 16:30:12 +0700

    disable register

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\StudentController;
 });*/
 
 Route::post('login', [ApiController::class, 'authenticate']);
-Route::post('register', [ApiController::class, 'register']);
+//Route::post('register', [ApiController::class, 'register']);
 
 Route::group(['middleware' => ['jwt.verify']], function() {
     Route::get('logout', [ApiController::class, 'logout']);
